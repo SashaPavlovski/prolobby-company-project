a2e4335152fb4baf27ea4cca162fdfb428fc4f1d
SashaPavlovski <111590243+SashaPavlovski@users.noreply.github.com>
2023-01-09 03:54:46 +0200

    Creating association pages

---
+++ CreatingAssociationPages/server/ProLobbyCompanyProject.Dal/SqlQueryClasses/SqlQuery.cs
@@ -0,0 +1,43 @@
+﻿using ProLobbyCompanyProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProLobbyCompanyProject.Dal
+{
+    public class SqlQuery
+    {
+        OpenConnection openConnection =  new OpenConnection();
+        public void CreateTables()
+        {
+            ConnectionWithSql._ConnectionWithSql.CreateTables();
+        }
+
+        public delegate object SetDataReader_delegate(SqlDataReader reader, SqlCommand command,string userID);
+
+        public object RunCommand(string sqlQuerey, SetDataReader_delegate func,string value, string userID)
+        {
+            if (!(openConnection.Connect())) return null;
+            object userList = null;
+
+            string insert = sqlQuerey;
+
+
+            using (SqlCommand command = new SqlCommand(insert, openConnection.connection))
+            {
+                if (value != null && userID != null && value.Contains("Campaigns_Id")) command.Parameters.AddWithValue($"@{value}", int.Parse(userID));
+                else if(value != null && userID != null) command.Parameters.AddWithValue($"@{value}", userID);
+                
+
+                using (SqlDataReader reader = command.ExecuteReader()) userList = func(reader, command, userID);
+            }
+            if (!(openConnection.CloseConnect())) return null;
+            return userList;
+        }
+
+    }
+}
---
+++ CreatingAssociationPages/server/ProLobbyCompanyProject.Dal/SqlQueryClasses/SqlQueryUpdate.cs
@@ -0,0 +1,36 @@
+﻿using ProLobbyCompanyProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProLobbyCompanyProject.Dal.SqlQueryClasses
+{
+    public class SqlQueryUpdate
+    {
+        public SqlQueryUpdate() { }
+        OpenConnection openConnection = new OpenConnection();
+
+        public void CreateTables()
+        {
+            ConnectionWithSql._ConnectionWithSql.CreateTables();
+        }
+
+        public delegate void UpdateUser_delegate(SqlCommand command, object UpdateData);
+        public void RunUpdateData(string sqlQuerey, UpdateUser_delegate func, object updateData)
+        {
+            if (!openConnection.Connect()) return;
+            string insert = sqlQuerey;
+
+            using (SqlCommand command = new SqlCommand(insert, openConnection.connection))
+            {
+                func(command, updateData);
+
+            }
+            if (!(openConnection.CloseConnect())) return;
+
+        }
+    }
+}
