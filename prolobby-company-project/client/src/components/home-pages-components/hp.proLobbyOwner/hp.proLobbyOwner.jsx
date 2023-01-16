71930fe63cc418839a0a2bc8d50de29796e3b855
SashaPavlovski <111590243+SashaPavlovski@users.noreply.github.com>
2023-01-17 01:24:43 +0200

    table reports

--- prolobby-company-project/client/src/components/home-pages-components/hp.proLobbyOwner/hp.proLobbyOwner.jsx
+++ prolobby-company-project/client/src/components/home-pages-components/hp.proLobbyOwner/hp.proLobbyOwner.jsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router-dom";
 
-//import { HomeOwner } from "../../../pages/home/home-owner/home-owner";
 import { ExistRowUser } from "../../repeat/user-if-exist-repeat";
 import { HomeAllCampaigns } from "./../../../pages/home/home-home-all-campaigns/home-all-campaigns";
 import { AboutCampaign } from "./../../../pages/organization/about-campaign/about-campaign";
 import { ProductsList } from "./../../../pages/products/products-list/products-list";
 import { SendCampaignsMoney } from "./../../../pages/send-campaigns-money/send-campaigns-money";
+import { ReportsCampaigns } from "./../../../pages/reports/reports-campaigns/reports-campaigns";
+import { ReportsPosts } from "./../../../pages/reports/reports-posts/reports-posts";
+import { ReportsUsers } from "./../../../pages/reports/reports-users/reports-users";
+import { ReportsProducts } from "./../../../pages/reports/reports-products/reports-products";
 
 export const ProLobbyOwnerHP = ({ components }) => {
   let { userData } = ExistRowUser();
@@ -20,6 +23,14 @@ export const ProLobbyOwnerHP = ({ components }) => {
       <ProductsList />
     ) : components === SendCampaignsMoney ? (
       <SendCampaignsMoney />
+    ) : components === ReportsCampaigns ? (
+      <ReportsCampaigns />
+    ) : components === ReportsPosts ? (
+      <ReportsPosts />
+    ) : components === ReportsUsers ? (
+      <ReportsUsers />
+    ) : components === ReportsProducts ? (
+      <ReportsProducts />
     ) : (
       <></>
     );
