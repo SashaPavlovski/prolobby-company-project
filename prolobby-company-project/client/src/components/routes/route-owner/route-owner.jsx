703769d0cbc0a09ac9f2bb036ec74accedfbff59
SashaPavlovski <111590243+SashaPavlovski@users.noreply.github.com>
2023-01-17 01:25:18 +0200

    table reports

--- prolobby-company-project/client/src/components/routes/route-owner/route-owner.jsx
+++ prolobby-company-project/client/src/components/routes/route-owner/route-owner.jsx
@@ -5,7 +5,11 @@ import { AboutCampaign } from "../../../pages/organization/about-campaign/about-
 import { ProductsList } from "../../../pages/products/products-list/products-list";
 import { ProLobbyOwnerHP } from "../../home-pages-components";
 import { LoginProLobbyOwner } from "../../../pages/login-components";
-import { SendCampaignsMoney } from './../../../pages/send-campaigns-money/send-campaigns-money';
+import { SendCampaignsMoney } from "./../../../pages/send-campaigns-money/send-campaigns-money";
+import { ReportsCampaigns } from './../../../pages/reports/reports-campaigns/reports-campaigns';
+import { ReportsPosts } from './../../../pages/reports/reports-posts/reports-posts';
+import { ReportsUsers } from './../../../pages/reports/reports-users/reports-users';
+import { ReportsProducts } from './../../../pages/reports/reports-products/reports-products';
 
 export const RoutesProLobbyOwner = () => {
   return (
@@ -28,6 +32,22 @@ export const RoutesProLobbyOwner = () => {
         <Route
           path="/send-active-money"
           element={<ProLobbyOwnerHP components={SendCampaignsMoney} />}
+        />
+        <Route
+          path="/reports/campaigns"
+          element={<ProLobbyOwnerHP components={ReportsCampaigns} />}
+        />
+        <Route
+          path="/reports/posts"
+          element={<ProLobbyOwnerHP components={ReportsPosts} />}
+        />
+        <Route
+          path="/reports/users"
+          element={<ProLobbyOwnerHP components={ReportsUsers} />}
+        />
+        <Route
+          path="/reports/products"
+          element={<ProLobbyOwnerHP components={ReportsProducts} />}
         />
       </Routes>
     </>
