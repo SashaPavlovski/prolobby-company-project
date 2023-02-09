a35bb30ad9c0fb4ad42662c8dee513c8d67a7dbd
SashaPavlovski <111590243+SashaPavlovski@users.noreply.github.com>
2023-02-09 22:11:43 +0200

    Making changes to Twitter and logs

--- prolobby-company-project/server/test/Program.cs
+++ prolobby-company-project/server/test/Program.cs
@@ -1,7 +1,12 @@
-﻿using ProLobbyCompanyProject.Dal;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
+using ProLobbyCompanyProject.Dal;
 using ProLobbyCompanyProject.Entites;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +18,24 @@ namespace test
     {
         static void Main(string[] args)
         {
-           
+ 
+            //Logger logger = LogManager.GetLogger("foo");
+            //logger.Info("started");
 
-           ConnectionWithSql._ConnectionWithSql.CreateTables();
+            //try
+            //{
+            //    throw new ArgumentException("test");
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    logger.Error(ex, "The test was successful");
+
+
+            //}
+           
+            //NLog.LogManager.Shutdown();
+          // ConnectionWithSql._ConnectionWithSql.CreateTables();
 
         }
     }
